339afc86eb3a7f870a30d36df9392c697ed9c5ee
Keshan Peiris <keshan.peiris@gmail.com>
2015-11-24 22:28:06 +1100

    Separating concerns like a boss

--- js/tictacJQ.js
+++ js/tictacJQ.js
@@ -16,4 +16,67 @@ var init = function (){
   moveCounter = 0;
 }
 
-//Playbutton event listener
+// creating playbutton event listener
+$("#startButton").click(function(){
+  if(haveWinner == 1){
+    init();
+  }
+  //connecting input to variable.(.val = get the current value within the element (this case input))
+  player1 = $("player-1-input").val();
+  player2 = $("player-2-input").val();
+  if (player1 == "" || player2 == ""){
+    alert("Please select names! ");
+    return;
+  }//startsGame
+  whoseTurn();
+});
+
+//adding functionality to the colums! (.col)
+$(".col").click(function(){
+  if (player1 == ""|| player2 = "");
+  }// .parent() is the parent of the element. .index() indicates the position of an element.
+  var row = $(this).parent().index();
+  var col = $(this).index();
+
+  if (grid[row][col] !== 0){
+    alert("Occupied area, Tic Tac Go somewhere else");
+    return;
+  }
+  if (haveWinner == 1){
+    alert("Click play again!");
+    return;
+  }
+  if (turn == player1){
+      //This counter is for calculating ties later (you can ignore)
+      moveCounter++;
+      $(this).text("O");
+  }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
